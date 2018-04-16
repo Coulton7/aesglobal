7a351c8e0e136220c1ea22c47715b6523a2ea311
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-04-16 16:18:00 +0100

    updated template with a piece of code to hopefully add img responsive

--- template.php
+++ template.php
@@ -3,22 +3,19 @@
  * @file
  * The primary PHP file for this theme.
  */
-
- 
- 
-function aesglobal_preprocess_page(&$vars) { 
-    // template files called page--contenttype.tpl.php 
-    if (isset($vars['node']->type)) { 
-        $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type; 
+function aesglobal_preprocess_page(&$vars) {
+    // template files called page--contenttype.tpl.php
+    if (isset($vars['node']->type)) {
+        $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
     }
-	
+
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
 }
 
 function aesglobal_theme(){
 	$items=array();
-	
+
 	$items['user_login']=array(
 	'render element' => 'form',
 	'path'=> drupal_get_path('theme', 'aesbs337').'/templates',
@@ -34,4 +31,8 @@ function aesglobal_preprocess_node(&$variables) {
   $node = $variables['node'];
   $date = format_date($node->created, 'custom', 'F j, Y');
   $variables['submitted'] = t('Submitted by !username on !datetime', array('!username' => $variables['name'], '!datetime' => $date));
+}
+
+function aesglobal_preprocess_image_style(&$vars) {
+	$vars['attributes']['class'][] = 'img-responsive';
 }
