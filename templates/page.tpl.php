38a4e07c5ab9244892291aa619b83c3eaad3ee78
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-04-23 14:39:44 +0100

    Missing question mark on php print title which is why is not appearing

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -226,7 +226,7 @@
 					<a id="main-content"></a>
 				<?php print render($title_prefix); ?>
 				<?php if (!empty($title)): ?>
-					<h1 class="page-header" id="page-main-heading"><php print $title; ?></h1>
+					<h1 class="page-header" id="page-main-heading"><?php print $title; ?></h1>
 				<?php endif; ?>
 
 				<?php print render($title_suffix); ?>
