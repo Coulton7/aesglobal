81ec449d4b0b15a661d8549bf616867b2396ca9e
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-04-27 15:32:43 +0100

    moved the col size to top of the breadcrumb so its all styled the same

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -205,13 +205,13 @@
   					<h1 class="page-header" id="page-main-heading"><?php print $title; ?></h1>
 					<?php endif; ?>
 
+			<div class="<?php if (empty($page['sidebar_first']) && empty($page['sidebar_second'])) { print 'col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2'; }
+														else if (empty($page['sidebar_first']) || empty($page['sidebar_second'])) { print 'col-sm-12'; }
+														else { print 'col-sm-12'; }
+											?>">
           <div class="breadcrumb-wrap">
             <div class="container-fluid">
               <div class="row">
-								<div class="<?php if (empty($page['sidebar_first']) && empty($page['sidebar_second'])) { print 'col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2'; }
-																	else if (empty($page['sidebar_first']) || empty($page['sidebar_second'])) { print 'col-sm-12'; }
-																	else { print 'col-sm-12'; }
-														?>">
                   <?php if (!empty($breadcrumb)): print t("You are here") . $breadcrumb; endif;?> <!--New breadcrumb location -->
 								</div>
               </div>
