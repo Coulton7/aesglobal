0a5b7f6197f67a05b14436d46c440cc0a1612274
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-30 12:12:58 +0100

    Removed the js to noty add too mouch weight to the site

--- templates/node--1270.tpl.php
+++ templates/node--1270.tpl.php
@@ -2,7 +2,6 @@
 
 <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script type="text/javascript" src="/sites/all/themes/aesbs337/js/modernizr.custom.min.js"></script>
 <script type="text/javascript" src="/sites/all/themes/aesbs337/js/modernizr.custom.26633.js"></script>
 <script type="text/javascript" src="/sites/all/themes/aesbs337/js/jquery.gridrotator.js"></script>
    <script type="text/javascript">
