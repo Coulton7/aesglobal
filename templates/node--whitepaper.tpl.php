fc1724984aa5e74f069884c74d9a71ae9bf203fc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-11-20 14:58:35 +0000

    Last attempt at getting the if statemnt on the div class md working

--- templates/node--whitepaper.tpl.php
+++ templates/node--whitepaper.tpl.php
@@ -88,8 +88,8 @@ $( document ).ready(function() {
 
 
 <div class="row">
-    <div class="<?php if ($conetnt['field_hubspot_form']['#items']['0']['value']) { print 'col-md-12'; }
-         else { print 'col-md-6'; } ?>">
+  <div class="<?php if (!empty($conetnt['field_hubspot_form'])) { print 'col-md-6'; }
+         else { print 'col-md-12'; } ?>">
         <?php
             // Hide comments, tags, and links now so that we can render them later.
             hide($content['comments']);
