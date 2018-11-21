bcebcc69410a403ca73aec0a250deafdab239922
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-11-21 15:45:02 +0000

    reset it broke the pages

--- templates/node--whitepaper.tpl.php
+++ templates/node--whitepaper.tpl.php
@@ -89,8 +89,7 @@ $( document ).ready(function() {
 
 <div class="row">
 
-  <div class="<?php if ($content['field_hubspot_form'])==NULL):{ print 'col-md-6'; }
-         else { print 'col-md-12'; } ?>">
+  <div class="col-md-6">
 
         <?php
             // Hide comments, tags, and links now so that we can render them later.
