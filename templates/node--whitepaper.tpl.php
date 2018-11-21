7ef60a4d4ff3ae6052c73f558badb38e20a9e429
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-11-21 15:23:30 +0000

    Testing $page instead of $content to see what effect it has on the page

--- templates/node--whitepaper.tpl.php
+++ templates/node--whitepaper.tpl.php
@@ -88,7 +88,10 @@ $( document ).ready(function() {
 
 
 <div class="row">
-    <div class="col-md-6">
+
+  <div class="<?php if (!empty($page['field_hubspot_form'])) { print 'col-md-6'; }
+         else { print 'col-md-12'; } ?>">
+
         <?php
             // Hide comments, tags, and links now so that we can render them later.
             hide($content['comments']);
