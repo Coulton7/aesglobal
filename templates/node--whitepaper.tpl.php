86b878518796acc6f444dab48494094a4b43c270
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-11-21 16:45:13 +0000

    New code variation based on another php if/else statement taregting it

--- templates/node--whitepaper.tpl.php
+++ templates/node--whitepaper.tpl.php
@@ -89,7 +89,8 @@ $( document ).ready(function() {
 
 <div class="row">
 
-  <div class="col-md-6">
+  <div class="<?php if($node->field_hubspot_form[0]['#value']): {print "col-md-6"}
+                    else{print"col-md-12"}?>">
 
         <?php
             // Hide comments, tags, and links now so that we can render them later.
