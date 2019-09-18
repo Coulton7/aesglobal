8c6d1e511dcc7dd9cb9edf12349288af6b44b061
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-09-18 17:05:59 +0100

    upping the image size

--- templates/views-view--clone-of-taxonomy-term--page-2.tpl.php
+++ templates/views-view--clone-of-taxonomy-term--page-2.tpl.php
@@ -33,15 +33,15 @@
     $(function() {
       var url = window.location.href;
       if (/NONE/.test(url)) {
-        $('#none').addClass('img-50');
+        $('#none').addClass('img-75');
       } else if (/IP55/.test(url)) {
-        $('#ip55').addClass('img-50');
+        $('#ip55').addClass('img-75');
       } else if (/IP66/.test(url)) {
-        $('#ip66').addClass('img-50');
+        $('#ip66').addClass('img-75');
       } else if (/IP69K/.test(url)) {
-        $('#ip69k').addClass('img-50');
+        $('#ip69k').addClass('img-75');
       } else {
-        $('#any').addClass('img-50');
+        $('#any').addClass('img-75');
       }
     });
   })
