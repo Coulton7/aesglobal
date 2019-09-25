592b1747c8328ebf1aa5be82815600b270336d29
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-09-25 15:32:20 +0100

    changing to true

--- templates/views-view--clone-of-taxonomy-term--page-2.tpl.php
+++ templates/views-view--clone-of-taxonomy-term--page-2.tpl.php
@@ -66,10 +66,13 @@
   <?php endif; ?>
 
   <?php
+		$url = request_uri();
+			if(strpos($url,'bearing-protection')==true){
       $block = block_load('block', '182');
 			$block_content = _block_render_blocks(array($block));
 			$build = _block_get_renderable_array($block_content);
 			print render($build);
+		}
     ?>
 
   <?php if ($exposed): ?>
