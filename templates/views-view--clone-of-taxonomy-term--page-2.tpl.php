a144abc8a3485dba54fe63e2338f7fbb581b490a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-12-17 10:25:18 +0000

    New script added to replace the inline scripting

--- templates/views-view--clone-of-taxonomy-term--page-2.tpl.php
+++ templates/views-view--clone-of-taxonomy-term--page-2.tpl.php
@@ -27,26 +27,7 @@
  * @ingroup views_templates
  */
 ?>
-<script type="text/javascript" charset="utf-8">
-(function($){
-	$(window).load(function() {
-    $(function() {
-      var url = window.location.href;
-      if (/NONE/.test(url)) {
-        $('#none').addClass('img-75');
-      } else if (/IP55/.test(url)) {
-        $('#ip55').addClass('img-75');
-      } else if (/IP66/.test(url)) {
-        $('#ip66').addClass('img-75');
-      } else if (/IP69K/.test(url)) {
-        $('#ip69k').addClass('img-75');
-      } else {
-        $('#any').addClass('img-75');
-      }
-    });
-  })
-})(jQuery);
-</script>
+<script type="text/javascript" src="/sites/all/themes/aesbs337/js/ip-filter.js"></script>
 
 <div class="<?php print $classes; ?>">
   <?php print render($title_prefix); ?>
