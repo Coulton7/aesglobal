88afb95e90daf7c88807bfcce9b2859faad07b40
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-12-17 09:47:05 +0000

    Added new script to replae the inline scripting on the page

--- templates/node--video.tpl.php
+++ templates/node--video.tpl.php
@@ -1,40 +1,6 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 <script type="text/javascript" src="/sites/all/themes/aesbs337/js/hubspot-progressive-profile.js"></script>
-
-<script>
-$( document ).ready(function() {
-  $('#block-block-166').hide();
-  $('.field-name-field-thumbnail').hide();
-
-  if ($('.field-name-field-hubspot-form').length > 0) {
-    $('.field-name-field-api-stream').hide();
-    $('.field-name-field-thumbnail').show();
-  }
-
-  function getUrlVars()
-    {
-      var vars = [], hash;
-      var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-      for(var i = 0; i < hashes.length; i++)
-      {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-      }
-      return vars;
-    }
-
-  var submitString = getUrlVars()["fs"];
-    if(submitString=='y'){
-      $('.oembed').show();
-      $('.field-name-field-thumbnail').hide();
-      $('.field-name-field-hubspot-form').hide();
-      $('#block-sharethis-sharethis-block').hide();
-      $('.field-label').hide();
-      $('#block-block-166').show();
-    }
-  });
-</script>
+<script type="text/javascript" src="/sites/all/themes/aesbs337/js/video-form-fill.js"></script>
 
   <header>
     <?php print render($title_prefix); ?>
