c6a891a20571cac571ae9ee3de1f422108c4f5b6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-12-23 09:56:53 +0000

    Restructured the maintenance mode tpl to be neater

--- templates/maintenance-page.tpl.php
+++ templates/maintenance-page.tpl.php
@@ -16,66 +16,117 @@
   <link rel="stylesheet" type="text/css" href="/sites/all/themes/aesseal_classic/css/ie7-and-down.css" />
 
 <![endif]-->
-
-  <?php print $scripts; ?>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 </head>
 
 <body class="<?php print $classes; ?>">
-    <div class="main-container container">
-    <div id="header">
-      <div id="logo-title">
 
-        <?php if (!empty($logo)): ?>
-          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-          </a>
-        <?php endif; ?>
+  <div class="container-fluid bannercontainer">
+	   <div class="row bannerimage">
+       <div class="row waveupper row-eq-height">
+         <div class="col-sm-2 col-xs-2 fullscreen">
+           <div class="wavelogo">
+             <img class="wavelogo" src="/sites/aesreliability.com/files/logo/logo.svg" alt ="logo"></img>
+           </div>
+           <img class= "uppercurve" src="/sites/all/themes/aesbs337/images/logos/wave-upper.svg" alt ="uppercurve"></img>
+           <?php if ($logo): ?>
+             <div class="logo navbar-btn pull-left" title="<?php print t('Home'); ?>">
+               <img src="/sites/aesreliability.com/files/secondary-logo/textonlylogo.svg" alt="<?php print t('Home'); ?> " class="textlogo img-adaptive" alt="image responsive"/>
+             </div>
+           <?php endif; ?>
+         </div>
 
-        <div id="name-and-slogan">
-          <?php if (!empty($site_name)): ?>
-            <h1 id="site-name">
-              <a href="<?php print $base_path ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
+         <div class="col-sm-10 col-xs-10 fullscreen">
+           <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
 
-          <?php if (!empty($site_slogan)): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /name-and-slogan -->
-      </div> <!-- /logo-title -->
+             <div class="<?php print $container_class; ?>">
+               <div class="navbar-header">
+               </div>
+             </div>
 
-      <?php if (!empty($header)): ?>
-        <div id="header-region">
-          <?php print $header; ?>
+             <div class="navbar-collapse">
+               <div class="container-fluid navbar-container">
+                 <nav role="navigation">
+                   <ul class="menu nav navbar-nav">
+                     <li class="first leaf">
+                       <a class="btn-disabled disabled" href="#">&nbsp;</a>
+                     </li>
+                   </nav>
+                 </div>
+               </div>
+               <div class="section-shadow-menu"></div>
+             </header>
+
+             <div id="name-and-slogan">
+               <?php if (!empty($site_name)): ?>
+                 <h1 id="site-name">
+                   <div title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></div>
+                 </h1>
+               <?php endif; ?>
+
+               <?php if (!empty($site_slogan)): ?>
+                 <div id="site-slogan"><?php print $site_slogan; ?></div>
+               <?php endif; ?>
+             </div>
+           </div>
+         </div>
+
+         <?php if (!empty($header)): ?>
+           <div id="header-region">
+             <?php print $header; ?>
+           </div>
+         <?php endif; ?>
+
+         <div class="preface front__flex-margin">
+           <div class="flex-gradient">
+             <div class="row">
+               <div class="col-sm-10 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+                 <div class="flex-header">
+                     <h1 class="page-header" id="page-main-heading">We are currently under maintenance</h1>
+                 </div>
+               </div>
+               <div class="col-lg-7 col-lg-offset-2 col-md-7 col-md-offset-1 col-sm-8">
+                 <div class="flex-caption">
+                 </div>
+               </div>
+             </div>
+           </div>
+     			</div>
         </div>
-      <?php endif; ?>
 
-    </div> <!-- /header -->
+        <div class="row wavelower row-eq-height">
+ 			      <div class="col-sm-9 col-xs-0 fullscreen">
+ 				         <div class="bottom-gap">
+ 				         </div>
+ 			      </div>
 
-    <div class="container" class="clearfix">
+ 			      <div class="col-sm-3 col-xs-12 fullscreen">
+ 				      <img class="curve-down" src = "/sites/all/themes/aesbs337/images/logos/wave-lower.svg" alt ="curve-down"></img>
+              <div class="socialmedia text-center">
+              </div>
+ 			        </div>
+ 		     </div>
+       </div>
 
-      <div id="main" class="column"><div id="main-squeeze">
+   <div class="container-fluid" class="clearfix">
+     <div class="row">
+       <div class="col-sm-10 col-lg-8 col-sm-offset-1 col-lg-offset-2">
+          <div id="main-squeeze">
+            <div id="content">
+              <?php if (!empty($title)): ?><h1 class="title" id="page-title"></h1><?php endif; ?>
+              <?php if (!empty($messages)): print $messages; endif; ?>
+              <div id="main-content" class="clearfix">
+                <?php print $content; ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+   </div>
 
-        <div id="content">
-          <?php if (!empty($title)): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-          <?php if (!empty($messages)): print $messages; endif; ?>
-          <div id="content-content" class="clearfix">
-            <?php print $content; ?>
-          </div> <!-- /content-content -->
-        </div> <!-- /content -->
-
-      </div></div> <!-- /main-squeeze /main -->
-
-
-    </div> <!-- /container -->
-
-    <div id="footer-wrapper">
-      <div id="footer">
-        <?php if (!empty($footer)): print $footer; endif; ?>
-      </div> <!-- /footer -->
-    </div> <!-- /footer-wrapper -->
-
-  </div> <!-- /page -->
+   <div id="footer-wrapper">
+     <div id="footer">
+      <?php if (!empty($footer)): print $footer; endif; ?>
+     </div>
+   </div>
 
 </body>
