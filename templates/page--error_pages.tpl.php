b73d507e9ad118d7e3f8b215922fc1936b2b2847
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-09-13 15:03:15 +0100

    Changed back to previous fontawesome embed for now

--- templates/page--error_pages.tpl.php
+++ templates/page--error_pages.tpl.php
@@ -87,7 +87,9 @@ if(!empty($file_fid)) {
         }
     }
 }?>
-<script src="https://kit.fontawesome.com/cd139cb838.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css" integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
 <div class="container-fluid bannercontainer">
 	<div class="row bannerimage">
 		<div class="row waveupper row-eq-height">
