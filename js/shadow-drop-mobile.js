d7e78c74468acfb7e96f7fc549f98c10e2719690
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-09-29 09:42:43 +0100

    Removing the scroll down effect of the netzero logo and text

--- js/shadow-drop-mobile.js
+++ js/shadow-drop-mobile.js
@@ -4,24 +4,4 @@
     $(".dropdown-menu.shadow-drop-menu:first-child").val($(this).text());
  });
 
-if ($(window).width() > 992) {
-  $(window).on("scroll", function() {
-   var lScroll = ($(this).scrollTop() / $(window).height())*90 - 100;
-   var tScroll = ($(this).scrollTop() / $(window).height())*80 - 75;
-   $('.net-zero-container').css({
-     'transform': 'translate( 25%,' + (lScroll) + '%)'
-   });
-   $('.mobile-carousel-caption').css({
-     'transform': 'translate( 0%,' + (tScroll) + '%)'
-   });
- });
-} else {
-  $('.net-zero-container').css({
-    'transform': 'translate( 0% 0%)'
-  });
-  $('.mobile-carousel-caption').css({
-    'transform': 'translate( 0% 0%)'
-  });
-}
-
 })(jQuery);
