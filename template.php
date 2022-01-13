b54d33992340923169a30bf4ec4991c04f1c2a35
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-01-13 15:46:42 +0000

    New template.php for page.tpl.php
    Added new php to template.php to allow for taxonomy vocabularies to have there on page.tpl.php's and to allow for a field to be used  for banner images so the why field can return in the header. More fields maybe required for images if none are used in repeat

--- template.php
+++ template.php
@@ -9,6 +9,24 @@ function aesglobal_preprocess_page(&$vars) {
         $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
     }
 
+    if (arg(0) == 'taxonomy' && arg(1) == 'term' ) {
+      $term = taxonomy_term_load(arg(2));
+      $vocabulary = taxonomy_vocabulary_load($term->vid);
+      $vars['theme_hook_suggestions'][] = 'page__taxonomy_vocabulary_' . $vocabulary->machine_name;
+    }
+
+    $term = menu_get_object('taxonomy_term', 2);
+    if ($term) {
+      $field_types_image = field_get_items('taxonomy_term', $term, 'field_types_image');
+      $field_industry_image = field_get_items('taxonomy_term', $term, 'field_industry_image');
+      if ($field_types_image) {
+        $vars['field_types_image'] = field_view_value('taxonomy_term', $term, 'field_types_image', $field_types_image[0]);
+      }
+      else if($field_industry_image) {
+        $vars['field_industry_image'] = field_view_value('taxonomy_term', $term, 'field_industry_image', $field_industry_image[0]);
+      }
+    }
+
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/smooth-scroll.js');
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
