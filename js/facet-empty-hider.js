256c583f5f0b9d6549bc1db476486465fe64237c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-01-18 14:58:49 +0000

    Adding new js which should hide facetwhen empty on searches

---
+++ js/facet-empty-hider.js
@@ -0,0 +1,9 @@
+(function($) {
+  $(document).ready(function() {
+    $('.block-facetapi').each(function() {
+      if($(this).children('facetapi-facetapi-links').length == 0) {
+        $(this).hide();
+      }
+    });
+  });
+})(jQuery);
